fe51a70e6aa8c7e1955f4e2229aceeeb0d9cd79a
Justin Patrick <jpatrick6420@gmail.com>
2024-03-12 07:39:21 -0600

    set site info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  site: "https://Jpatrick6420.github.io",
+
+  integrations: [react(), tailwind()],
 });
